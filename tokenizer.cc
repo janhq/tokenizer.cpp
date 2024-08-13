b9ae9668d2c0da6835923375c3eff571c6544271
vansangpfiev <sang@jan.ai>
2024-08-13 16:45:16 +0700

    fix: clean code

--- tokenizer.cc
+++ tokenizer.cc
@@ -1,5 +1,79 @@
 #include "tokenizer.h"
 
 namespace tokenizer_cpp {
-
+SentencePieceTokenizer::SentencePieceTokenizer(const std::string& model_path) {
+  auto status = processor_.Load(model_path);
+  if (!status.ok()) {
+    std::cerr << status.ToString() << std::endl;
+  }
+  std::cout << "Successully loaded the tokenizer" << std::endl;
 }
+
+std::string SentencePieceTokenizer::DecodeWithSpace(const int id) {
+  std::string text = processor_.IdToPiece(id);
+  ReplaceSubstring(text, "▁", " ");
+  return text;
+}
+
+std::string SentencePieceTokenizer::Decode(const std::vector<int32_t> ids) {
+  std::string text = processor_.DecodeIds(ids);
+  return text;
+}
+
+std::vector<int> SentencePieceTokenizer::Encode(const std::string& input) {
+  std::vector<int> ids;
+  processor_.Encode(input, &ids);
+  return ids;
+}
+
+void SentencePieceTokenizer::ReplaceSubstring(std::string& base,
+                                              const std::string& from,
+                                              const std::string& to) {
+  size_t start_pos = 0;
+  while ((start_pos = base.find(from, start_pos)) != std::string::npos) {
+    base.replace(start_pos, from.length(), to);
+    start_pos += to.length();
+  }
+}
+
+class TFilePathResourceReader : public IResourceReader {
+ public:
+  TFilePathResourceReader(const std::string& path) : path_(path) {}
+
+  std::vector<std::string> readLines() override {
+    std::ifstream file(path_);
+    if (!file.is_open()) {
+      throw std::runtime_error("Embedded resource '" + path_ + "' not found.");
+    }
+
+    std::string line;
+    std::vector<std::string> lines;
+    while (std::getline(file, line)) {
+      lines.push_back(line);
+    }
+
+    return lines;
+  }
+
+ private:
+  std::string path_;
+};
+
+TiktokenTokenizer::TiktokenTokenizer(const std::string& model_path) {
+  TFilePathResourceReader reader(model_path);
+  encoder_ =
+      GptEncoding::get_encoding_llama3(LanguageModel::CL100K_BASE, &reader);
+  std::cout << "Successully loaded the tokenizer" << std::endl;
+}
+
+std::string TiktokenTokenizer::Decode(const std::vector<int32_t> ids) {
+  std::string text = encoder_->decode(ids);
+  return text;
+}
+
+std::vector<int> TiktokenTokenizer::Encode(const std::string& input) {
+  std::vector<int> ids = encoder_->encode(input);
+  return ids;
+}
+
+}  // namespace tokenizer_cpp
--- tokenizer.h
+++ tokenizer.h
@@ -9,31 +9,7 @@
 
 namespace tokenizer_cpp {
 // Class Tokenizer is not thread-safe
-class TFilePathResourceReader : public IResourceReader {
- public:
-  TFilePathResourceReader(const std::string& path) : path_(path) {}
-
-  std::vector<std::string> readLines() override {
-    std::ifstream file(path_);
-    if (!file.is_open()) {
-      throw std::runtime_error("Embedded resource '" + path_ + "' not found.");
-    }
-
-    std::string line;
-    std::vector<std::string> lines;
-    while (std::getline(file, line)) {
-      lines.push_back(line);
-    }
-
-    return lines;
-  }
-
- private:
-  std::string path_;
-};
-
 class Tokenizer {
-
  public:
   Tokenizer() {}
   virtual ~Tokenizer() {}
@@ -48,66 +24,32 @@ class Tokenizer {
 class SentencePieceTokenizer : public Tokenizer {
 
  public:
-  SentencePieceTokenizer(const std::string& model_path) : Tokenizer() {
-    auto status = processor.Load(model_path);
-    if (!status.ok()) {
-      std::cerr << status.ToString() << std::endl;
-    }
-    std::cout << "Successully loaded the tokenizer" << std::endl;
-  }
+  SentencePieceTokenizer(const std::string& model_path);
 
-  std::string DecodeWithSpace(const int id) override {
-    std::string text = processor.IdToPiece(id);
-    ReplaceSubstring(text, "▁", " ");
-    return text;
-  }
+  std::string DecodeWithSpace(const int id) final;
 
-  std::string Decode(const std::vector<int32_t> ids) override {
-    std::string text = processor.DecodeIds(ids);
-    return text;
-  }
+  std::string Decode(const std::vector<int32_t> ids) final;
 
-  std::vector<int> Encode(const std::string& input) override {
-    std::vector<int> ids;
-    processor.Encode(input, &ids);
-    return ids;
-  }
+  std::vector<int> Encode(const std::string& input) final;
 
  private:
   void ReplaceSubstring(std::string& base, const std::string& from,
-                        const std::string& to) {
-    size_t start_pos = 0;
-    while ((start_pos = base.find(from, start_pos)) != std::string::npos) {
-      base.replace(start_pos, from.length(), to);
-      start_pos += to.length();
-    }
-  }
+                        const std::string& to);
 
  private:
-  sentencepiece::SentencePieceProcessor processor;
+  sentencepiece::SentencePieceProcessor processor_;
 };
 
 class TiktokenTokenizer : public Tokenizer {
  public:
-  TiktokenTokenizer(const std::string& model_path) : Tokenizer() {
-    TFilePathResourceReader reader(model_path);
-    encoder =
-        GptEncoding::get_encoding_llama3(LanguageModel::CL100K_BASE, &reader);
-    std::cout << "Successully loaded the tokenizer" << std::endl;
-  }
+  TiktokenTokenizer(const std::string& model_path);
 
-  std::string Decode(const std::vector<int32_t> ids) override {
-    std::string text = encoder->decode(ids);
-    return text;
-  }
+  std::string Decode(const std::vector<int32_t> ids) final;
 
-  std::vector<int> Encode(const std::string& input) override {
-    std::vector<int> ids = encoder->encode(input);
-    return ids;
-  }
+  std::vector<int> Encode(const std::string& input) final;
 
  private:
-  std::shared_ptr<GptEncoding> encoder;
+  std::shared_ptr<GptEncoding> encoder_;
 };
 
 }  // namespace tokenizer_cpp
